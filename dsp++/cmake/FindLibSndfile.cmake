c72acacd412f3cced5dd167a272c889c4f428de4
Andrzej Ciarkowski <andrzej.ciarkowski@gmail.com>
2015-10-11 13:31:09 +0200

    CMakeLists.txt: added cmake module for finding libsndfile

---
+++ dsp++/cmake/FindLibSndfile.cmake
@@ -0,0 +1,46 @@
+# - Try to find libsndfile
+# Once done this will define
+#  LIBSNDFILE_FOUND - System has libsndfile
+#  LIBSNDFILE_INCLUDE_DIRS - The libsndfile include directories
+#  LIBSNDFILE_LIBRARIES - The libraries needed to use libsndfile
+#  LIBSNDFILE_DEFINITIONS - Compiler switches required for using libsndfile
+
+if (MSVC)
+	option(LIBSNDFILE_ROOT "Installation folder of libsndfile" $ENV{LIBSNDFILE_ROOT})
+
+	if (CMAKE_CL_64)
+		set(LIBSNDFILE_HINTS ${LIBSNDFILE_ROOT}/x64)
+	else ()
+		set(LIBSNDFILE_DIR ${LIBSNDFILE_ROOT}/x86)
+	endif()
+
+	find_library(LIBSNDFILE_LIBRARY NAMES libsndfile libsndfile-1 libsndfile1 sndfile sndfile-1 sndfile1
+		HINTS ${LIBSNDFILE_DIR}/lib )
+
+	find_path(LIBSNDFILE_INCLUDE_DIR sndfile.h
+		HINTS ${LIBSNDFILE_DIR}/include
+		PATH_SUFFIXES sndfile )
+
+else ()
+	find_package(PkgConfig)
+	pkg_check_modules(PC_LIBSNDFILE QUIET libsndfile)
+	set(LIBSNDFILE_DEFINITIONS ${PC_LIBSNDFILE_CFLAGS_OTHER})
+
+	find_path(LIBSNDFILE_INCLUDE_DIR sndfile.h
+          HINTS ${PC_LIBSNDFILE_INCLUDEDIR} ${PC_LIBSNDFILE_INCLUDE_DIRS}
+          PATH_SUFFIXES sndfile )
+
+	find_library(LIBSNDFILE_LIBRARY NAMES libsndfile sndfile
+             HINTS ${PC_LIBSNDFILE_LIBDIR} ${PC_LIBSNDFILE_LIBRARY_DIRS} )
+
+	set(LIBSNDFILE_LIBRARIES ${LIBSNDFILE_LIBRARY} )
+	set(LIBSNDFILE_INCLUDE_DIRS ${LIBSNDFILE_INCLUDE_DIR} )
+endif()
+
+include(FindPackageHandleStandardArgs)
+# handle the QUIETLY and REQUIRED arguments and set LIBSNDFILE_FOUND to TRUE
+# if all listed variables are TRUE
+find_package_handle_standard_args(LibSndfile  DEFAULT_MSG
+                                  LIBSNDFILE_LIBRARY LIBSNDFILE_INCLUDE_DIR)
+
+mark_as_advanced(LIBSNDFILE_INCLUDE_DIR LIBSNDFILE_LIBRARY )
