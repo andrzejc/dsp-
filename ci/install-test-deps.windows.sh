22edfa524fedb6892303da30bf5b9be5eb7f2c30
Andrzej Ciarkowski <andrzej.ciarkowski@gmail.com>
2020-02-29 20:12:15 +0100

    Download boost release on our own, choco installer is sloow

--- ci/install-test-deps.windows.sh
+++ ci/install-test-deps.windows.sh
@@ -1,14 +1,15 @@
 choco install \
     openssl.light \
-    doxygen.install \
-    boost-msvc-14.1
+    doxygen.install
 
 case "${TARGET_PLATFORM}" in
 *64)
+    bin/winstall-exe https://sourceforge.net/projects/boost/files/boost-binaries/1.72.0/boost_1_72_0-msvc-14.1-64.exe f49cd0c11976a9bb4e5edd3e8111e61fd0b859ac /VERYSILENT
     winstall-libsndfile "${TARGET_PLATFORM}" http://www.mega-nerd.com/libsndfile/files/libsndfile-1.0.28-w64-setup.exe 3783e513d735d1526f19a32a63991026
     winstall-fftw3 "${TARGET_PLATFORM}" ftp://ftp.fftw.org/pub/fftw/fftw-3.3.5-dll64.zip 409f4a5272506eb7422d265a68a02deeefcb5c17
     ;;
 *in32|*86)
+    bin/winstall-exe https://sourceforge.net/projects/boost/files/boost-binaries/1.72.0/boost_1_72_0-msvc-14.1-32.exe 121e069354cc792a3b1bb42fe7285e5380beea45 /VERYSILENT
     winstall-libsndfile "${TARGET_PLATFORM}" http://www.mega-nerd.com/libsndfile/files/libsndfile-1.0.28-w32-setup.exe 443a2a2890969778e8f9fe6a146c0595
     winstall-fftw3 "${TARGET_PLATFORM}" ftp://ftp.fftw.org/pub/fftw/fftw-3.3.5-dll32.zip b19d875b07a0e4ac8251b0aa072321582374e000
     ;;
