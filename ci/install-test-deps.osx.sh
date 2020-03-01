0ba5131904704b190eba3b908271a5a53b6cbf36
Andrzej Ciarkowski <andrzej.ciarkowski@gmail.com>
2020-03-01 01:18:58 +0100

    Revert installing deps via addon on osx - this fails

--- ci/install-test-deps.osx.sh
+++ ci/install-test-deps.osx.sh
@@ -1,12 +1,12 @@
-# HOMEBREW_NO_AUTO_UPDATE=1 \
-# HOMEBREW_NO_INSTALL_CLEANUP=1 \
-#     brew install \
-#         ccache \
-#         cmake \
-#         libsndfile \
-#         fftw \
-#         boost \
-#         doxygen \
-#         graphviz
+HOMEBREW_NO_AUTO_UPDATE=1 \
+HOMEBREW_NO_INSTALL_CLEANUP=1 \
+    brew install \
+        ccache \
+        cmake \
+        libsndfile \
+        fftw \
+        boost \
+        doxygen \
+        graphviz
 
 export PATH="/usr/local/opt/ccache/libexec:$PATH"
