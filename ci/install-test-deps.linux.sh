2eeb4e228837b16793fdc06a8221a9045bbea477
Andrzej Ciarkowski <andrzej.ciarkowski@gmail.com>
2020-03-01 01:01:06 +0100

    Install deps via addons on linux/osx

--- ci/install-test-deps.linux.sh
+++ ci/install-test-deps.linux.sh
@@ -1,8 +1,8 @@
-sudo apt-get update
-sudo apt-get install \
-    cmake \
-    libsndfile1-dev \
-    libfftw3-dev \
-    libboost-dev \
-    doxygen \
-    graphviz
+# sudo apt-get update
+# sudo apt-get install \
+#     cmake \
+#     libsndfile1-dev \
+#     libfftw3-dev \
+#     libboost-dev \
+#     doxygen \
+#     graphviz
--- ci/install-test-deps.osx.sh
+++ ci/install-test-deps.osx.sh
@@ -1,9 +1,11 @@
-HOMEBREW_NO_AUTO_UPDATE=1 \
-HOMEBREW_NO_INSTALL_CLEANUP=1 \
-    brew install \
-        cmake \
-        libsndfile \
-        fftw \
-        boost \
-        doxygen \
-        graphviz
+# HOMEBREW_NO_AUTO_UPDATE=1 \
+# HOMEBREW_NO_INSTALL_CLEANUP=1 \
+#     brew install \
+#         cmake \
+#         libsndfile \
+#         fftw \
+#         boost \
+#         doxygen \
+#         graphviz
+
+export PATH="/usr/local/opt/ccache/libexec:$PATH"
