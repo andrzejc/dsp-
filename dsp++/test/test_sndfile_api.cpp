1fd6d36141f565bbd546bc55f70ce3f9381ff8f3
Andrzej Ciarkowski <andrzej.ciarkowski@gmail.com>
2020-03-25 03:31:39 +0100

    WIP

--- dsp++/test/test_sndfile_api.cpp
+++ dsp++/test/test_sndfile_api.cpp
@@ -5,7 +5,6 @@
 #include <gtest/gtest.h>
 
 #include <cstdio>
-#include <filesystem>
 
 namespace dsp { namespace snd { namespace sndifle {
 
@@ -43,11 +42,6 @@ TEST(sndfile_api, file_usable) {
     EXPECT_EQ(fmt.sample_type(), sample::type::ieee_float);
     EXPECT_EQ(fmt.sample_bits(), 32);
     EXPECT_EQ(fmt.type(), file_type::label::wav);
-
-    // ASSERT_TRUE(f.property(property::title));
-    // EXPECT_EQ(*f.property(property::title), "f32_48k");
-    // ASSERT_TRUE(f.property(property::artist));
-    // EXPECT_EQ(*f.property(property::artist), "dsp++");
 }
 
 TEST(sndfile_api, reader_usable) {
@@ -98,6 +92,38 @@ TEST(sndfile_api, channel_layout_preserved_on_rewrite) {
     r.open(temp_aiff_file, &rf);
     EXPECT_EQ(rf.type(), file_type::label::aiff);
     EXPECT_EQ(rf.channel_layout(), channel::layout::s5_0);
+    r.close();
+    std::remove(temp_aiff_file);
+}
+
+TEST(sndfile_api, properties_preserved_on_rewrite) {
+    sndfile::reader r;
+    file_format rf;
+    r.open(test_data("s16_48k.wav").c_str(), &rf);
+    ASSERT_EQ(rf.channel_layout(), channel::layout::undefined);
+    ASSERT_EQ(r.frame_count(), 4800);
+    file_format wf = rf;
+    wf.set_type(file_type::label::aiff);
+    wf.set_channel_layout(channel::layout::stereo);
+
+    char temp_aiff_file[L_tmpnam];
+    std::tmpnam(temp_aiff_file);
+    sndfile::writer w;
+    w.open(temp_aiff_file, &wf);
+    w.set_property(property::title, "s16_48k");
+    ASSERT_TRUE(w.is_open());
+    float buf[5 * 480];
+    for (int i = 0; i < 10; ++i) {
+        EXPECT_EQ(r.read_frames(buf, 480), 480);
+        EXPECT_EQ(w.write_frames(buf, 480), 480);
+    }
+    w.close();
+    r.close();
+
+    r.open(temp_aiff_file, &rf);
+    EXPECT_EQ(rf.type(), file_type::label::aiff);
+    EXPECT_TRUE(r.property(property::title));
+    EXPECT_EQ(*r.property(property::title), "s16_48k");
     r.close();
     std::remove(temp_aiff_file);
 }
