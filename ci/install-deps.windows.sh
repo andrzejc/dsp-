cb23b61436748070ab4d6c06be9e7a49471299ca
Andrzej Ciarkowski <andrzej.ciarkowski@gmail.com>
2020-04-03 14:38:55 +0200

    Update build-scripts to support mpg123 on Windows

--- ci/install-deps.windows.sh
+++ ci/install-deps.windows.sh
@@ -7,11 +7,13 @@ case "${TARGET_PLATFORM}" in
     winstall-exe https://sourceforge.net/projects/boost/files/boost-binaries/1.72.0/boost_1_72_0-msvc-14.1-64.exe f49cd0c11976a9bb4e5edd3e8111e61fd0b859ac /VERYSILENT
     winstall-libsndfile "${TARGET_PLATFORM}" http://www.mega-nerd.com/libsndfile/files/libsndfile-1.0.28-w64-setup.exe 3783e513d735d1526f19a32a63991026
     winstall-fftw3 "${TARGET_PLATFORM}" ftp://ftp.fftw.org/pub/fftw/fftw-3.3.5-dll64.zip 409f4a5272506eb7422d265a68a02deeefcb5c17
+    winstall-mpg123 "${TARGET_PLATFORM}" http://mpg123.de/download/win64/1.25.13/mpg123-1.25.13-x86-64.zip 809e847de9169e63b65d1593d163977c2fca5a65
     ;;
 *in32|*86)
     winstall-exe https://sourceforge.net/projects/boost/files/boost-binaries/1.72.0/boost_1_72_0-msvc-14.1-32.exe 121e069354cc792a3b1bb42fe7285e5380beea45 /VERYSILENT
     winstall-libsndfile "${TARGET_PLATFORM}" http://www.mega-nerd.com/libsndfile/files/libsndfile-1.0.28-w32-setup.exe 443a2a2890969778e8f9fe6a146c0595
     winstall-fftw3 "${TARGET_PLATFORM}" ftp://ftp.fftw.org/pub/fftw/fftw-3.3.5-dll32.zip b19d875b07a0e4ac8251b0aa072321582374e000
+    winstall-mpg123 "${TARGET_PLATFORM}" http://mpg123.de/download/win32/1.25.13/mpg123-1.25.13-x86.zip 4af3c0e278dab84e9d2c503c30a0517aa5d40205
     ;;
 *)
     >&2 echo "Installation of libsndfile for target '${TARGET_PLATFORM}' is not supported"
