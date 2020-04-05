a9bc4fad2142250a83435b0600ad3dd2a7d675fc
Andrzej Ciarkowski <andrzej.ciarkowski@gmail.com>
2020-04-05 15:45:58 +0200

    Revert to not updating homebrew

--- ci/install-deps.osx.sh
+++ ci/install-deps.osx.sh
@@ -1,16 +1,17 @@
-# HOMEBREW_NO_AUTO_UPDATE=1 \
-# HOMEBREW_NO_INSTALL_CLEANUP=1 \
-#     brew install \
-#         ccache \
-#         cmake \
-#         ninja \
-#         mpg123 \
-#         lame \
-#         libsndfile \
-#         fdk-aac \
-#         fftw \
-#         boost \
-#         doxygen \
-#         graphviz
+HOMEBREW_NO_AUTO_UPDATE=1 \
+HOMEBREW_NO_INSTALL_CLEANUP=1 \
+    brew install \
+        ccache \
+        cmake \
+        ninja \
+        mpg123 \
+        lame \
+        libsndfile \
+        fdk-aac \
+        fftw \
+        doxygen \
+        graphviz
+
+        # boost \
 
 export PATH="/usr/local/opt/ccache/libexec:$PATH"
