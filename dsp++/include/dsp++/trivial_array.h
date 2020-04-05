b38f2a09bd19bf9e548893808f7e89828d580152
Andrzej Ciarkowski <andrzej.ciarkowski@gmail.com>
2020-04-05 15:29:13 +0200

    Get rid of std::allocator deprecated in C++17 in trivial_array.h

--- dsp++/include/dsp++/trivial_array.h
+++ dsp++/include/dsp++/trivial_array.h
@@ -3,49 +3,31 @@
  * @brief Implementation of a simple, noncopyable array template capable of using custom memory allocator.
  * @author Andrzej Ciarkowski <mailto:andrzej.ciarkowski@gmail.com>
  */
-#ifndef DSP_TRIVIAL_ARRAY_H_INCLUDED
-#define DSP_TRIVIAL_ARRAY_H_INCLUDED
-
-#include <dsp++/export.h>
-#include <dsp++/noncopyable.h>
+#pragma once
+#include <dsp++/types.h>
 
 #include <memory>
-#include <cstddef>
-
 
 namespace dsp {
 
 struct uninitialized_t {};
-extern const uninitialized_t uninitialized;
+constexpr uninitialized_t uninitialized = {};
 
-template<class Elem, class Alloc = std::allocator<Elem> >
-class trivial_array: private noncopyable
-{
-	typedef typename Alloc::template rebind<Elem>::other this_allocator;
-
-	void destroy()
-	{
-		this_allocator a(alloc_);
-		if (init_) {
-			pointer p = arr_;
-			for (size_t i = 0; i < size_; ++i, ++p)
-				a.destroy(p);
-			init_ = false;
-		}
-		a.deallocate(arr_, size_);
-	}
+template<class Elem, class Alloc = std::allocator<Elem>>
+class trivial_array {
+    using this_allocator = typename std::allocator_traits<Alloc>::template rebind_alloc<Elem>;
 
 public:
-	typedef Alloc 								allocator_type;
-	typedef Elem 								value_type;
-    typedef typename this_allocator::pointer           	pointer;
-    typedef typename this_allocator::const_pointer     	const_pointer;
-    typedef typename this_allocator::reference         	reference;
-    typedef typename this_allocator::const_reference   	const_reference;
-    typedef pointer 							iterator;
-    typedef const_pointer 						const_iterator;
-    typedef size_t					 			size_type;
-    typedef ptrdiff_t					 		difference_type;
+    using allocator_type = Alloc;
+    using value_type = Elem;
+    using pointer = typename std::allocator_traits<this_allocator>::pointer;
+    using const_pointer = typename std::allocator_traits<this_allocator>::const_pointer;
+    using reference = Elem&;
+    using const_reference = const Elem&;
+    using iterator = pointer;
+    using const_iterator = const_pointer;
+    using size_type = size_t;
+    using difference_type = ptrdiff_t;
 
     pointer get() {return arr_;}
     const_pointer get() const {return arr_;}
@@ -58,32 +40,42 @@ public:
     reference operator[](size_type n) {return arr_[n];}
     const_reference operator[](size_type n) const {return arr_[n];}
 
-    explicit trivial_array(size_t size, const value_type& val = value_type(), const allocator_type& a = allocator_type())
-     :	alloc_(a)
-     ,	arr_(this_allocator(alloc_).allocate(size))
-     ,	size_(size)
-     ,	init_(false)
-    {std::uninitialized_fill_n(arr_, size, val); init_ = true;}
-
-    explicit trivial_array(size_t size, const uninitialized_t&, const allocator_type& a = allocator_type())
-     :	alloc_(a)
-     ,	arr_(this_allocator(alloc_).allocate(size))
-     ,	size_(size)
-     ,	init_(false)
+    explicit trivial_array(size_t size, const uninitialized_t&, const allocator_type& a = allocator_type()):
+        alloc_(a),
+        arr_(std::allocator_traits<this_allocator>::allocate(alloc_, size)),
+        size_(size),
+        init_(false)
     {}
 
-    ~trivial_array() {destroy();}
+    explicit trivial_array(size_t size, const value_type& val = value_type(), const allocator_type& a = allocator_type()):
+        trivial_array{size, uninitialized, a}
+    {
+        std::uninitialized_fill_n(arr_, size, val);
+        init_ = true;
+    }
+
+    trivial_array(const trivial_array&) = delete;
+    trivial_array& operator=(const trivial_array&) = delete;
+
+    ~trivial_array() {
+        if (init_) {
+            pointer p = arr_;
+            for (size_t i = 0; i < size_; ++i, ++p) {
+                std::allocator_traits<this_allocator>::destroy(alloc_, p);
+            }
+            init_ = false;
+        }
+        std::allocator_traits<this_allocator>::deallocate(alloc_, arr_, size_);
+    }
 
     size_type size() const {return size_;}
     size_type length() const {return size_;}
 
 private:
-    allocator_type alloc_;
-	pointer	arr_;
-	size_type size_;
-	bool init_;
+    this_allocator alloc_;
+    pointer	arr_;
+    size_type size_;
+    bool init_;
 };
 
 }
-
-#endif /* DSP_TRIVIAL_ARRAY_H_INCLUDED */
