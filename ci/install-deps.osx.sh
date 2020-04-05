65cf535b66820e5ae9b106c7358dc4514d942778
Andrzej Ciarkowski <andrzej.ciarkowski@gmail.com>
2020-04-05 12:56:11 +0200

    Preparing to switch to full C++17 compatibility

--- ci/install-deps.osx.sh
+++ ci/install-deps.osx.sh
@@ -1,16 +1,16 @@
-HOMEBREW_NO_AUTO_UPDATE=1 \
-HOMEBREW_NO_INSTALL_CLEANUP=1 \
-    brew install \
-        ccache \
-        cmake \
-        ninja \
-        mpg123 \
-        lame \
-        libsndfile \
-        fdk-aac \
-        fftw \
-        boost \
-        doxygen \
-        graphviz
+# HOMEBREW_NO_AUTO_UPDATE=1 \
+# HOMEBREW_NO_INSTALL_CLEANUP=1 \
+#     brew install \
+#         ccache \
+#         cmake \
+#         ninja \
+#         mpg123 \
+#         lame \
+#         libsndfile \
+#         fdk-aac \
+#         fftw \
+#         boost \
+#         doxygen \
+#         graphviz
 
 export PATH="/usr/local/opt/ccache/libexec:$PATH"
