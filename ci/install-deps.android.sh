bd4717bd8d43e084b81852c09441ae2f5df3304c
Andrzej Ciarkowski <andrzej.ciarkowski@gmail.com>
2021-02-20 21:08:38 +0100

    Add all libsndfile deps on Android

--- ci/install-deps.android.sh
+++ ci/install-deps.android.sh
@@ -29,6 +29,16 @@ wget -c -nv https://android-deps.s3-us-west-1.amazonaws.com/fftw-3.3.9.tar.bz2
 tar xf fftw-3.3.9.tar.bz2 --strip-components=1
 wget -c -nv https://android-deps.s3-us-west-1.amazonaws.com/libsndfile-1.0.31.tar.bz2
 tar xf libsndfile-1.0.31.tar.bz2 --strip-components=1
+wget -c -nv https://android-deps.s3-us-west-1.amazonaws.com/flac-1.3.3.tar.bz2
+tar xf flac-1.3.3.tar.bz2 --strip-components=1
+wget -c -nv https://android-deps.s3-us-west-1.amazonaws.com/libogg-1.3.4.tar.bz2
+tar xf libogg-1.3.4.tar.bz2 --strip-components=1
+wget -c -nv https://android-deps.s3-us-west-1.amazonaws.com/libvorbis-1.3.7.tar.bz2
+tar xf libvorbis-1.3.7.tar.bz2 --strip-components=1
+wget -c -nv https://android-deps.s3-us-west-1.amazonaws.com/opus-1.3.1.tar.bz2
+tar xf opus-1.3.1.tar.bz2 --strip-components=1
+wget -c -nv https://android-deps.s3-us-west-1.amazonaws.com/speex-1.2.0.tar.bz2
+tar xf speex-1.2.0.tar.bz2 --strip-components=1
 wget -c -nv https://android-deps.s3-us-west-1.amazonaws.com/mpg123-1.26.4.tar.bz2
 tar xf mpg123-1.26.4.tar.bz2 --strip-components=1
 wget -c -nv https://android-deps.s3-us-west-1.amazonaws.com/lame-3.100.tar.bz2
